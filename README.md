2c0e41fa68feed6631c1248878ff366e92471911
Tom de Bruijn <tom@tomdebruijn.com>
2016-11-12 17:40:00 +0100

    Install AppSignal

---
+++ README.md
@@ -0,0 +1,26 @@
+# AppSignal + Rails 5 + Sequel
+
+> This an example application. Please read this repositories [main
+  README](../../blob/master/README.md) and then visit back here.
+
+This example app demonstrates how to integrate the [appsignal][appsignal-gem]
+gem with [Rails 5][rails] and [Sequel][sequel].
+
+The Rails application uses the bare minimum of Rails frameworks for this
+example. ActiveRecord is not present in this application.
+
+## Usage
+
+```
+$ bundle exec rails s
+```
+
+Visit these URLs:
+
+- http://localhost:3000/
+- http://localhost:3000/slow
+- http://localhost:3000/error
+
+[appsignal-gem]: https://github.com/appsignal/appsignal-ruby
+[rails]: http://rubyonrails.org/
+[sequel]: http://sequel.jeremyevans.net/
--- config/initializers/sequel.rb
+++ config/initializers/sequel.rb
@@ -1,14 +1,18 @@
-require "sequel"
-DB = Sequel.sqlite
-
-DB.create_table :items do
-  primary_key :id
-  String :name
-  Float :price
+unless defined? DB
+  DB = Sequel.sqlite
+  Sequel::Model.db = DB
 end
 
-items = DB[:items]
-items.insert(:name => "Soap", :price => 1)
-items.insert(:name => "Water", :price => 1234.567)
-items.insert(:name => "Cheese", :price => 2.95)
-items.insert(:name => "Stroopwaffles", :price => 10)
+unless DB.table_exists? :items
+  DB.create_table :items do
+    primary_key :id
+    String :name
+    Float :price
+  end
+
+  items = DB[:items]
+  items.insert(:name => "Soap", :price => 1)
+  items.insert(:name => "Water", :price => 1234.567)
+  items.insert(:name => "Cheese", :price => 2.95)
+  items.insert(:name => "Stroopwaffles", :price => 10)
+end
