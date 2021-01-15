933f26a0a4dcef0d73771028d827324cbd610d2c
Tom de Bruijn <tom@tomdebruijn.com>
2021-01-15 15:33:16 +0100

    Update Shoryuken example app
    Update gems to more recent versions.
    Update the example worker to not call `shoryuken_options` multiple
    times, that registers the worker multiple times. This was highlighted
    when I tried to add `batch: true` as an option to one of the calls and
    it raised an error.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source "https://rubygems.org"
 
 gem "shoryuken"
+gem "aws-sdk-sqs"
 gem "appsignal"
--- worker.rb
+++ worker.rb
@@ -12,11 +12,10 @@ Appsignal.start_logger
 class MyWorker
   include Shoryuken::Worker
 
-  shoryuken_options queue: "default", auto_delete: true
+  shoryuken_options queue: "default", auto_delete: true, body_parser: :json
 
   # shoryuken_options body_parser: ->(sqs_msg){ REXML::Document.new(sqs_msg.body) }
   # shoryuken_options body_parser: :json
-  shoryuken_options body_parser: JSON
 
   def perform(sqs_msg, body)
     puts sqs_msg
